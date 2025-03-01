7cbfc1b9e8d7a0a2736b987ca795215fbb234f46
augustoalves-13 <augustoalves.sep@gmail.com>
2025-03-01 03:23:38 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(50, 420);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(250, 50);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Adicionar Task";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTask
+            // 
+            this.txtTask.AccessibleDescription = "";
+            this.txtTask.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtTask.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtTask.ForeColor = System.Drawing.Color.Black;
+            this.txtTask.Location = new System.Drawing.Point(50, 50);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(500, 30);
+            this.txtTask.TabIndex = 1;
+            this.txtTask.Enter += new System.EventHandler(this.txtTask_Enter);
+            this.txtTask.Leave += new System.EventHandler(this.txtTask_Leave);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lstTasks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 20;
+            this.lstTasks.Location = new System.Drawing.Point(50, 100);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(500, 242);
+            this.lstTasks.TabIndex = 2;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.Crimson;
+            this.btnRemove.FlatAppearance.BorderSize = 0;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnRemove.ForeColor = System.Drawing.Color.White;
+            this.btnRemove.Location = new System.Drawing.Point(300, 420);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(250, 50);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remover Task";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(600, 500);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "TaskManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        private string placeholderText = "Digite aqui sua tarefa...";
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtTask.Text = placeholderText;
+            txtTask.ForeColor = Color.Gray;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void txtTask_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                txtTask.Text = placeholderText;
+                txtTask.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtTask_Enter(object sender, EventArgs e)
+        {
+            if (txtTask.Text == placeholderText)
+            {
+                txtTask.Text = "";
+                txtTask.ForeColor = Color.Black;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string task = txtTask.Text.Trim();
+            if (!string.IsNullOrEmpty(task))
+            {
+                lstTasks.Items.Add(task);
+                txtTask.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Digite uma tarefa para adicionar", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedItem != null)
+            {
+                lstTasks.Items.Remove(lstTasks.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma tarefa para remover", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
